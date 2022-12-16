a3e2365e44a82e8378ac8bcb6ca6d7cbbc846e25
rotich98 <emmanuelkipkemei13@gmail.com>
2022-12-16 03:51:39 -0800

    Task 1

--- binary_trees.h
+++ binary_trees.h
@@ -41,5 +41,6 @@ typedef struct levelorder_queue_s
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
